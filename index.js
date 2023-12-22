8995d6eba0e503c39133b07112f204562d0f4f27
dCOMMICS <sanwurld@gmail.com>
2023-12-22 13:51:08 +0300

    initial commit

--- index.js
+++ index.js
@@ -41,3 +41,43 @@ async function fetchAndShowResult(url){
 }
 
 
+//  create movie card html template
+
+function createMovieCard(movie) {
+    const { posterPath, originalTitle, ReleaseDate, overview } = movie;
+
+    const imagePath = posterPath ? imgApi + posterPath : "./img-01.jpeg";
+
+    const truncatedTitle = originalTitle.length > 15 ?
+    originalTitle.slice(0,15) + "..." : originalTitle ;
+
+    const formattedDate = ReleaseDate || " No release date";
+
+    const cardTemplate = `
+            <div class="column"> 
+            <div class = "row">
+            <a class = "card-media" href = "./img-01.jpeg">
+
+            <img src = "${imagePath}" alt = "{originalTitle}" width = "100%" />
+
+            </a>
+
+            <div class = "card-content">
+                <div class = " card-header">
+                        <div class = "left-content">
+
+                        <h3 style = " font-weight: 600 "> $ {truncatedTitle}</h3>
+                        <span style = "color: #12efec"> ${formattedDate} </span>
+                        </div>
+                        <div class = " right-content"> 
+                        <a href = $ {imagePath}" target = "_blank" class = " card-btn">See Cover </a>
+                         </div>
+                         </div>
+
+                         <div class = " info">
+                         $ {overview || "No Overview yet....." } 
+    `
+
+    
+}
+
